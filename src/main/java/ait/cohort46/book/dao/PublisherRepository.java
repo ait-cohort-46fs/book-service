6e36ebb63bbad000ccaaeb63754f595edaf1c52d
edward <edwardlfish@gmail.com>
2024-10-29 11:48:15 +0200

    First commit

---
+++ src/main/java/ait/cohort46/book/dao/PublisherRepository.java
@@ -0,0 +1,7 @@
+package ait.cohort46.book.dao;
+
+import ait.cohort46.book.model.Publisher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PublisherRepository extends JpaRepository<Publisher, String> {
+}
